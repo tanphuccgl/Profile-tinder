9b140d8c3b46a57bdfeeb049f0b1c17003ea1605
Levi <phamnguyentanphucgl@gmail.com>
2023-08-02 10:07:34 +0700

    add comment

--- app/src/main/java/com/example/profiletinder/GridViewAdapter.java
+++ app/src/main/java/com/example/profiletinder/GridViewAdapter.java
@@ -9,42 +9,50 @@ import android.widget.EditText;
 
 import java.util.List;
 
+// Lớp GridViewAdapter là một custom adapter dùng để cung cấp dữ liệu cho GridView trong ứng dụng Android.
+
 public class GridViewAdapter extends BaseAdapter {
 
-    private Context mContext;
-    private List<String> mData;
+    private Context mContext; // Context của ứng dụng.
+    private List<String> mData; // Danh sách dữ liệu để hiển thị trong GridView.
 
+    // Constructor của lớp GridViewAdapter, được sử dụng để khởi tạo dữ liệu cho adapter.
     public GridViewAdapter(Context context, List<String> data) {
-        mContext = context;
-        mData = data;
+        mContext = context; // Gán Context được truyền vào cho biến mContext.
+        mData = data; // Gán danh sách dữ liệu được truyền vào cho biến mData.
     }
 
+    // Phương thức getCount() được ghi đè từ BaseAdapter để trả về số lượng phần tử trong danh sách dữ liệu.
     @Override
     public int getCount() {
         return mData.size();
     }
 
+    // Phương thức getItem() được ghi đè từ BaseAdapter để trả về phần tử tại vị trí position trong danh sách dữ liệu.
     @Override
     public Object getItem(int position) {
         return mData.get(position);
     }
 
+    // Phương thức getItemId() được ghi đè từ BaseAdapter để trả về ID của phần tử tại vị trí position trong danh sách dữ liệu.
     @Override
     public long getItemId(int position) {
         return position;
     }
 
-
-
+    // Phương thức getView() được ghi đè từ BaseAdapter để tạo và trả về View cho mỗi phần tử trong GridView.
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Kiểm tra nếu convertView chưa tồn tại (null), thì tạo mới bằng cách inflate layout grid_item2.
         if (convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_item2, parent, false);
         }
 
+        // Lấy tham chiếu đến EditText trong convertView, sau đó thiết lập văn bản cho EditText từ danh sách dữ liệu mData tại vị trí position.
         EditText editText = convertView.findViewById(R.id.editText1);
         editText.setText(mData.get(position));
 
+        // Trả về convertView chứa thông tin của phần tử tại vị trí position trong danh sách dữ liệu.
         return convertView;
     }
 }
--- app/src/main/java/com/example/profiletinder/PurposeDialogFragment.java
+++ app/src/main/java/com/example/profiletinder/PurposeDialogFragment.java
@@ -7,25 +7,39 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
+// Lớp PurposeDialogFragment là một Fragment trong Android dùng để hiển thị hộp thoại (Dialog) cho người dùng chọn mục đích hẹn hò.
+
 public class PurposeDialogFragment extends DialogFragment {
 
+    // Mảng PURPOSES chứa các mục đích hẹn hò để hiển thị trong danh sách lựa chọn trong hộp thoại.
     private static final String[] PURPOSES = {"Tìm bạn đời", "Tìm người yêu", "Kết bạn mới", "Hẹn hò nghiêm túc"};
 
+    // Interface OnPurposeSelectedListener dùng để giao tiếp từ Fragment tới Activity chứa nó khi người dùng chọn mục đích hẹn hò.
     public interface OnPurposeSelectedListener {
         void onPurposeSelected(String purpose);
     }
 
+    // Phương thức onCreateDialog(Bundle savedInstanceState) được ghi đè để tạo hộp thoại AlertDialog và trả về nó.
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        // Tạo một đối tượng AlertDialog.Builder để xây dựng hộp thoại.
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+
+        // Đặt tiêu đề cho hộp thoại.
         builder.setTitle("Chọn mục đích hẹn hò")
+
+                // Đặt danh sách các mục chọn vào hộp thoại, sử dụng mảng PURPOSES và xử lý sự kiện chọn mục bằng một lambda expression.
                 .setItems(PURPOSES, (dialog, which) -> {
+                    // Lấy giá trị của mục được chọn từ mảng PURPOSES.
                     String selectedPurpose = PURPOSES[which];
+
+                    // Lấy interface OnPurposeSelectedListener từ Activity chứa Fragment và gọi phương thức onPurposeSelected() với giá trị mục đích hẹn hò đã chọn.
                     OnPurposeSelectedListener listener = (OnPurposeSelectedListener) requireActivity();
                     listener.onPurposeSelected(selectedPurpose);
                 });
 
+        // Trả về hộp thoại AlertDialog đã tạo.
         return builder.create();
     }
 }
